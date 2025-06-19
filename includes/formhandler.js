8464cf88429abebd83c8f934bb9b29f206665a46
Loravis <lora.loravis@gmail.com>
2025-06-19 12:08:42 +0200

    Fixed form validation not working correctly.

--- includes/formhandler.js
+++ includes/formhandler.js
@@ -1,3 +1,8 @@
+function isInteger(str) {
+  const num = Number(str);
+  return Number.isInteger(num);
+}
+
 // Add or remove a danger border to an element
 function setDangerBorder(form, elementName, set) {
     if (set) {
@@ -16,13 +21,28 @@ function validateAddRoomForm() {
     let capacity = form["add_new_capacity"].value;
     
     // Check if any required fields were not filled out
-    let missing = false;
-    missing = setDangerBorder(form, "add_new_roomnr", roomnr == "");
-    missing = setDangerBorder(form, "add_new_floor", floor == "");
-    missing = setDangerBorder(form, "add_new_capacity", capacity == "");
+    let roomnr_missing = false;
+    let floor_missing = false;
+    let capacity_missing = false;
+    roomnr_missing = setDangerBorder(form, "add_new_roomnr", roomnr == "");
+    floor_missing = setDangerBorder(form, "add_new_floor", floor == "");
+    capacity_missing = setDangerBorder(form, "add_new_capacity", capacity == "");
 
-    if (missing) {
+    if (roomnr_missing || floor_missing || capacity_missing) {
         document.getElementById("add_new_room_error").innerHTML = "Sie haben noch nicht alle erforderlichen Felder ausgefüllt!";
+        return false;
+    }
+
+    // Check if any non-integer values were entered
+    let roomnr_invalidval = false;
+    let floor_invalidval = false;
+    let capacity_invalidval = false;
+    roomnr_invalidval = setDangerBorder(form, "add_new_roomnr", !isInteger(roomnr));
+    floor_invalidval = setDangerBorder(form, "add_new_floor", !isInteger(floor));
+    capacity_invalidval = setDangerBorder(form, "add_new_capacity", !isInteger(capacity));
+
+    if (roomnr_invalidval || floor_invalidval || capacity_invalidval) {
+        document.getElementById("add_new_room_error").innerHTML = "Ungültige Angaben! Nur ganze Zahlen dürfen angegeben werden.";
         return false;
     }
 
@@ -35,14 +55,29 @@ function validateEditRoomForm() {
     let floor = form["edit_new_floor"].value;
     let capacity = form["edit_new_capacity"].value;
     
-    // Check if any required fields were left empty
-    let missing = false;
-    missing = setDangerBorder(form, "edit_new_roomnr", roomnr == "");
-    missing = setDangerBorder(form, "edit_new_floor", floor == "");
-    missing = setDangerBorder(form, "edit_new_capacity", capacity == "");
+    // Check if any required fields were not filled out
+    let roomnr_missing = false;
+    let floor_missing = false;
+    let capacity_missing = false;
+    roomnr_missing = setDangerBorder(form, "edit_new_roomnr", roomnr == "");
+    floor_missing = setDangerBorder(form, "edit_new_floor", floor == "");
+    capacity_missing = setDangerBorder(form, "edit_new_capacity", capacity == "");
 
-    if (missing) {
+    if (roomnr_missing || floor_missing || capacity_missing) {
         document.getElementById("edit_new_room_error").innerHTML = "Sie haben noch nicht alle erforderlichen Felder ausgefüllt!";
+        return false;
+    }
+
+    // Check if any non-integer values were entered
+    let roomnr_invalidval = false;
+    let floor_invalidval = false;
+    let capacity_invalidval = false;
+    roomnr_invalidval = setDangerBorder(form, "edit_new_roomnr", !isInteger(roomnr));
+    floor_invalidval = setDangerBorder(form, "edit_new_floor", !isInteger(floor));
+    capacity_invalidval = setDangerBorder(form, "edit_new_capacity", !isInteger(capacity));
+
+    if (roomnr_invalidval || floor_invalidval || capacity_invalidval) {
+        document.getElementById("edit_new_room_error").innerHTML = "Ungültige Angaben! Nur ganze Zahlen dürfen angegeben werden.";
         return false;
     }
 
