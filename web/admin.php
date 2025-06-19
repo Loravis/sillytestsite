bb4e6779f12f97ee50172d1a99ad666e014719b2
Loravis <lora.loravis@gmail.com>
2025-06-19 10:51:07 +0200

    Added setDangerBorder(), to improve code cleanliness.

--- web/admin.php
+++ web/admin.php
@@ -58,33 +58,28 @@
             }
         </style>
         <script>
+            // Add or remove a danger border to an element
+            function setDangerBorder(form, elementName, set) {
+                if (set) {
+                    form[elementName].classList.add("border-danger");
+                    return true;
+                } else {
+                    form[elementName].classList.remove("border-danger");
+                    return false;
+                }
+            }
+
             function validateAddRoomForm() {
                 let form = document.forms["add_new_form"];
                 let roomnr = form["add_new_roomnr"].value;
                 let floor = form["add_new_floor"].value;
                 let capacity = form["add_new_capacity"].value;
                 
+                // Check if any required fields were not filled out
                 let missing = false;
-                if (roomnr == "") {
-                    form["add_new_roomnr"].classList.add("border-danger");
-                    missing = true;
-                } else {
-                    form["add_new_roomnr"].classList.remove("border-danger");
-                }
-
-                if (floor == "") {
-                    form["add_new_floor"].classList.add("border-danger");
-                    missing = true;
-                } else {
-                    form["add_new_floor"].classList.remove("border-danger");
-                }
-
-                if (capacity == "") {
-                    form["add_new_capacity"].classList.add("border-danger");
-                    missing = true;
-                } else {
-                    form["add_new_capacity"].classList.remove("border-danger");
-                }
+                missing = setDangerBorder(form, "add_new_roomnr", roomnr == "");
+                missing = setDangerBorder(form, "add_new_floor", floor == "");
+                missing = setDangerBorder(form, "add_new_capacity", capacity == "");
 
                 if (missing) {
                     document.getElementById("add_new_room_error").innerHTML = "Sie haben noch nicht alle erforderlichen Felder ausgefüllt!";
