bc582cb3e96d3bdb410c2b1a0a2b33a56bf6f381
Loravis <lora.loravis@gmail.com>
2025-06-19 12:34:20 +0200

    Implemented loading of current values into the edit form.

--- includes/tablebuttonhandler.js
+++ includes/tablebuttonhandler.js
@@ -1,8 +1,14 @@
-function setFormValues() {
-    let form = document.forms["open_edit_modal"];
-    let roomnr = form["roomnr"].value;
+function setFormValues(button) {
+    const form = button.closest("form");
+    const roomnr = form.querySelector('input[name="roomnr"]').value;
+    const floor = form.querySelector('input[name="floor"]').value;
+    const capacity = form.querySelector('input[name="capacity"]').value;
     
     let edit_form = document.forms["edit_new_form"];
     edit_form["edit_new_roomnr"].value = roomnr;
+    edit_form["edit_new_floor"].value = floor;
+    edit_form["edit_new_capacity"].value = capacity;
     return false;
 }
+
+// TODO: Implement server side edit handling
